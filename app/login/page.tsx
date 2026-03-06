8386e85fa46ea8504145ab10c06c95fdabed66f9
Rengy <rengyprivatelimited@gmail.com>
2026-03-06 16:21:41 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
@@ -108,12 +109,7 @@ export default function LoginPage() {
       <section className="mx-auto flex min-h-[calc(100vh-4rem)] w-full max-w-[760px] items-center justify-center bg-[#f8f8f8] p-6 sm:p-10">
         <div className="w-full max-w-[360px] text-center">
           <div className="mx-auto mb-8 flex h-24 w-24 items-center justify-center">
-            <svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M40.8125 24H24V40.8125L19.7969 36.6094V19.7969H34.5V15.5938L40.8125 24Z" fill="#121212" />
-              <path d="M24 55.1875H40.8125V38.375L45.0156 42.5781V59.3906H30.3125V63.5938L24 55.1875Z" fill="#121212" />
-              <path d="M37.7109 50.4219C32.1094 50.4219 30.7344 47.8789 29.8125 45.2656C29.2773 43.7461 29.1484 43.3867 27.2695 43.3867V39.3516C31.9922 39.3516 33.0039 42.207 33.6094 43.9102C34.1953 45.5586 34.4883 46.3867 37.7109 46.3867V50.4219Z" fill="#121212" />
-              <path d="M45 23.9531V35.7617L48.8789 41.6133V23.9531H45ZM45 37.2422L44.1328 35.9336L38.9688 28.1211L36.2266 23.9531H32.3477V24.1953L36.2266 30.0469L40.4688 36.4609L45 43.293V43.5H48.8789V43.2891L45 37.2422Z" fill="#121212" />
-            </svg>
+            <Image src="/logo.png" alt="Rengy logo" width={96} height={96} priority />
           </div>
 
           <h1 className="text-4xl font-semibold tracking-[-0.02em] text-[#080808] sm:text-[48px]">Welcome Team Member</h1>
